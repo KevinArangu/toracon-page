43b3eddd575162f83ea8890f49866e17fa1133d9
Ing. Kevin Arangu <kevin.arangu@gmail.com>
2022-07-29 07:38:04 -0400

    Cambios en Google Tag Body

--- src/components/common/Heads/GoogleTagBody.js
+++ src/components/common/Heads/GoogleTagBody.js
@@ -2,8 +2,7 @@ const GoogleTagBody = () => {
   return (
     <noscript
       dangerouslySetInnerHTML={{
-        __html: `<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5V9RVN9"
-          height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>`,
+        __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5V9RVN9" height="0" width="0" style="display:none;visibility:hidden"></iframe>`
       }}
     />
   )
