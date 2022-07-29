240211c31d7e4524050a60a78de188cad4e2a475
Ing. Kevin Arangu <kevin.arangu@gmail.com>
2022-07-28 21:25:54 -0400

    Primeras configs

---
+++ src/components/layout/MainLayout/MainLayout.js
@@ -0,0 +1,8 @@
+
+const MainLayout = ({children}) => {
+  return (
+    <main>{children}</main>
+  )
+}
+
+export default MainLayout
---
+++ src/hooks/useExample.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const useExample = () => {
+  return (
+    <div>useExample</div>
+  )
+}
+
+export default useExample
