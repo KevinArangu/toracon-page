1135679140e2388c0c612a9e6dafec32c23a94d3
Kevin Arangu <38361709+KevinArangu@users.noreply.github.com>
2022-08-03 23:13:22 -0400

    Correcciones Home

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,15 @@ import Counter from "@/components/common/Counter"
 import Container from "@/components/layout/Container";
 
 export default function Home() {
-  return
-  <>
-    <div className="h-72 w-full bg-gray-300">HERO</div>
-    <Container className="space-y-4 mt-4">
-      <div className="h-48 w-full bg-gray-200">CONTENT</div>
-      <div className="h-48 w-full bg-gray-200">CONTENT</div>
-      <div className="h-48 w-full bg-gray-200">CONTENT</div>
-    </Container>
-  </>
+  return (
+    <>
+      <div className="h-72 w-full bg-gray-300">HERO</div>
+
+      <Container className="space-y-4 mt-4">
+        <div className="h-48 w-full bg-gray-200">CONTENT</div>
+        <div className="h-48 w-full bg-gray-200">CONTENT</div>
+        <div className="h-48 w-full bg-gray-200">CONTENT</div>
+      </Container>
+    </>
+  )
 }
