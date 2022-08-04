44cc5936d68d90a5dee75d103c92b9d256558de6
Kevin Arangu <38361709+KevinArangu@users.noreply.github.com>
2022-08-03 23:36:04 -0400

    Corrigiendo estilos y agregando mas rutas

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,3 @@
-import Counter from "@/components/common/Counter"
 import Container from "@/components/layout/Container";
 
 export default function Home() {
@@ -8,17 +7,22 @@ export default function Home() {
 
       <Container className="space-y-8 my-8">
         <div className="h-72 w-full bg-gray-200 rounded-md flex flex-col lg:space-x-4 lg:flex-row">
-          <div className="w-96 h-full bg-gray-300 rounded-md">IMAGE</div>
+          <div className="w-full lg:w-96 h-full bg-gray-300 rounded-md">IMAGE</div>
           <div className="flex-1">TEXT</div>
-        </div>
-
-        <div className="h-72 w-full bg-gray-200 rounded-md flex flex-col lg:space-x-4 lg:flex-row-reverse">
-          <div className="flex-1">TEXT</div>
-          <div className="w-96 h-full bg-gray-300 rounded-md">IMAGE</div>
         </div>
 
         <div className="h-72 w-full bg-gray-200 rounded-md flex flex-col lg:space-x-4 lg:flex-row">
-          <div className="w-96 h-full bg-gray-300 rounded-md">IMAGE</div>
+          <div className="flex-1">TEXT</div>
+          <div className="w-full lg:w-96 h-full bg-gray-300 rounded-md">IMAGE</div>
+        </div>
+
+        <div className="h-72 w-full bg-gray-200 rounded-md flex flex-col lg:space-x-4 lg:flex-row">
+          <div className="w-full lg:w-96 h-full bg-gray-300 rounded-md">IMAGE</div>
+          <div className="flex-1">TEXT</div>
+        </div>
+        
+        <div className="h-72 w-full bg-gray-200 rounded-md flex flex-col lg:space-x-4 lg:flex-row">
+          <div className="w-full lg:w-96 h-full bg-gray-300 rounded-md">IMAGE</div>
           <div className="flex-1">TEXT</div>
         </div>
       </Container>
---
+++ src/pages/nosotros/index.js
@@ -0,0 +1,16 @@
+import Container from "@/components/layout/Container";
+
+export default function AboutUs() {
+  return (
+    <>
+      <div className="h-80 w-full bg-gray-300">SOBRE NOSOTROS</div>
+
+      <Container className="space-y-8 my-8">
+        <div className="h-72 w-full bg-gray-200 rounded-md flex flex-col lg:space-x-4 lg:flex-row">
+          <div className="w-full lg:w-96 h-full bg-gray-300 rounded-md">IMAGE</div>
+          <div className="flex-1">TEXT</div>
+        </div>
+      </Container>
+    </>
+  )
+}
