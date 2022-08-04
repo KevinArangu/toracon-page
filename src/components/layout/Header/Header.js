e9e8da7b78a201e240775598f4d45f1d6ec5e457
Kevin Arangu <38361709+KevinArangu@users.noreply.github.com>
2022-08-03 22:20:22 -0400

    Prueba con el logo en el Header

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -1,13 +1,15 @@
 import { MainHeader } from './styles';
 import Container from "@/components/layout/Container";
+import Image from "next/image"
 
 const Header = () => {
   return (
-    <MainHeader>
-      <Container>
-        Toracon
+      <Container $as={MainHeader}>
+        <div className="max-w-[12rem]">
+          <Image src="iso.svg" fill="responsive" width={150} height={150} className="fill-white"  />
+        </div>
+        <div className="flex-1"></div>
       </Container>
-    </MainHeader>
   )
 }
 
